ad8d7ba063818111329195a5ed4009680fc28bbd
RIGIK93 <maxim.peniaz@gmail.com>
2024-05-13 11:26:16 -0400

    Missing Axolotl JPG and InfoButton added

---
+++ src/lib/components/InfoButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let modal: any;
+</script>
+
+<button class="btn btn-square btn-xs" on:click={() => modal.showModal()}><img src="https://img.icons8.com/info" class="not-prose h-4 w-4" alt="learn more"/></button>
+<dialog bind:this={modal} class="modal modal-bottom sm:modal-middle not-prose">
+    <div class="modal-box">
+      <slot/>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+</dialog>
